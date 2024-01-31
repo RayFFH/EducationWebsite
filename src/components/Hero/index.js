a82ab5771df5dafdbebc8e90924db2a98a836722
RayFFH <57190209+RayFFH@users.noreply.github.com>
2024-01-31 20:49:48 +0000

    first commit

---
+++ src/components/Hero/index.js
@@ -0,0 +1,15 @@
+import "./index.css";
+import heroimage from "../../assets/1.jpg"
+
+function Hero (){
+    return(<>
+        <div className ="hero">
+            <img alt = "heroimage"src={heroimage}/>
+
+
+            </div>
+      
+        </>);
+}
+
+export default Hero;
---
+++ src/components/routes/Home.js
@@ -0,0 +1,16 @@
+import Navbar from "../Navbar";
+import Hero from "../Hero";
+
+
+function Home (){
+    return(
+        <>
+        <Navbar />
+        <h1>This is Home</h1>
+        <Hero/>
+        </>
+
+    )
+}
+
+export default Home;
